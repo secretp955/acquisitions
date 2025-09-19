ca57ab375084eff5114fd4beb115d07e42273be2
Ka Ho <hokaho824@gmail.com>
2025-09-19 18:41:24 +0800

    Secure out API with Arcjet

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import morgan from "morgan";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRoutes from "#routes/auth.routes.js";
-import {timestamp} from "drizzle-orm/pg-core";
+import securityMiddleware from "#middleware/security.middleware.js";
 
 const app = express();
 
@@ -16,6 +16,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(morgan("combined", { stream: { write: (message) => logger.info(message.trim()) }}));
+
+app.use(securityMiddleware);
 
 app.get('/', (req, res) => {
   logger.info('Hello from Acquisitions!');
